b3c1e0f9162c1080eb68bcb5cfdb64d3aeb896bd
vladmog <vladmog@gmail.com>
2019-07-24 14:16:08 -0700

    Set-up Redux and moved App.js to redux store

--- src/App.js
+++ src/App.js
@@ -4,23 +4,24 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { connect } from 'react-redux';
 
-const App = () => {
+const App = (props) => {
   const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    store: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
+    // additionalPrice: 0,
+    // car: {
+    //   price: 26395,
+    //   name: '2019 Ford Mustang',
+    //   image:
+    //     'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+    //   features: []
+    // },
+    // store: [
+    //   { id: 1, name: 'V-6 engine', price: 1500 },
+    //   { id: 2, name: 'Racing detail package', price: 1500 },
+    //   { id: 3, name: 'Premium sound system', price: 500 },
+    //   { id: 4, name: 'Rear spoiler', price: 250 }
+    // ]
   };
 
   const removeFeature = item => {
@@ -34,15 +35,24 @@ const App = () => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures store={props.store} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+function mapStateToProps(state){
+  console.log("state:", state)
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    store: state.store,
+  }
+}
+
+export default connect(mapStateToProps)(App);
---
+++ src/reducers/reducer.js
@@ -0,0 +1,29 @@
+import {ADD_TODO} from '../actions/actions';
+
+const defaultState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    store: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+}
+
+export default function reducer (state = defaultState, action) {
+    switch (action.type){
+        case ADD_TODO:
+            return {
+                ...state
+            }
+        default:
+            return state;
+    }
+}
